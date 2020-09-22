025c01d9676e157034b384d601fd5a2dc2103db9
hvboq <50200488+hvboq@users.noreply.github.com>
2020-09-22 18:20:55 +0900

    [Add] DB CRUD중 CR 구현

--- AriticleApp.Models/Articles/ArticleRepository.cs
+++ AriticleApp.Models/Articles/ArticleRepository.cs
@@ -1,6 +1,9 @@
 ﻿using Dul.Domain.Common;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace AriticleApp.Models
@@ -10,11 +13,39 @@ namespace AriticleApp.Models
     /// </summary>
     public class ArticleRepository : IArticleRepository
     {
-        public Task<Article> AddArticleAsync(Article article)
+        ArticleAppDBContext _context;
+        public ArticleRepository(ArticleAppDBContext context)
         {
-            throw new NotImplementedException();
+            this._context = context;
+        }
+        //입력
+        public async Task<Article> AddArticleAsync(Article article)
+        {
+            _context.Articles.Add(article);
+            await _context.SaveChangesAsync();
+            return article;
+        }
+        // 페이징
+        public async Task<PagingResult<Article>> GetAllAsync(int pageIndex, int pageSize)
+        {
+            var totalRecords = await _context.Articles.CountAsync();
+            var articles = await _context.Articles
+                .OrderByDescending(a => a.Id)
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return new PagingResult<Article>(articles, totalRecords);
         }
 
+        public Task<List<Article>> GetArticlesAsync()
+        {
+            return _context.Articles.OrderByDescending(a => a.Id).ToListAsync();
+        }
+        public async Task<Article> GetArticleByIdAsync(int id)
+        {
+            return await _context.Articles.FindAsync(id);
+        }
         public Task DeleteArticleAsync(int i)
         {
             throw new NotImplementedException();
@@ -25,19 +56,7 @@ namespace AriticleApp.Models
             throw new NotImplementedException();
         }
 
-        public Task<PagingResult<Article>> GetAllAsync(int pageIndex, int pageSize)
-        {
-            throw new NotImplementedException();
-        }
 
-        public Task<Article> GetArticleByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<Article>> GetArticlesAsync()
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }
