025f359f35472d8991107b7bd406fdda2446d14e
hvboq <50200488+hvboq@users.noreply.github.com>
2020-10-07 10:31:00 +0900

    [Add] 종속성 주입

--- ArticleApp/Startup.cs
+++ ArticleApp/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ArticleApp.Areas.Identity;
 using ArticleApp.Data;
+using AriticleApp.Models;
+using Microsoft.Extensions.Options;
 
 namespace ArticleApp
 {
@@ -33,6 +35,21 @@ namespace ArticleApp
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddSingleton<WeatherForecastService>();
+            AddDependencyInjectionContainerForArticles(services);
+        }
+        
+
+        /// <summary>
+        /// 게시판 관련 의존성 주입 관련 코드만 따로 모아서 관리
+        /// </summary>
+
+        private void AddDependencyInjectionContainerForArticles(IServiceCollection services)
+        {
+            // ArticleAppDBContext.cs Inject: New DbContext Add
+            services.AddEntityFrameworkSqlServer().AddDbContext<ArticleAppDBContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            // IArticleRepository.cs Inject: Di Container에 서비스(리포지토리) 등록
+            services.AddTransient<IArticleRepository, ArticleRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
