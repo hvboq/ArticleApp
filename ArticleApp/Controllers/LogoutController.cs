3868cc54dc3917039da4549e5b989c308a8c5c7c
hvboq <50200488+hvboq@users.noreply.github.com>
2020-12-04 14:08:59 +0900

    [Add] Logout 기능 구현

---
+++ ArticleApp/Controllers/LogoutController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArticleApp.Controllers
+{
+    public class LogoutController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Index()
+        {
+            // 로그아웃
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return LocalRedirect("/");
+        }
+    }
+}
